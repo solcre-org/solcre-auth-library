71ca5f4b93fc6ce64723714df277e11355e36095
Gustavo Rodriguez <gustavo.rodriguez@solcre.com>
2020-04-27 16:20:36 -0300

    fix(publicAPI): remove login component reference

--- projects/solcre-org/ng-auth/src/public-api.ts
+++ projects/solcre-org/ng-auth/src/public-api.ts
@@ -1,8 +1,6 @@
 /*
  * Public API Surface of solcre-auth-library
  */
-
-export * from './lib/login/login.component';
 export * from './lib/auth.service';
 export * from './lib/ng-auth.module';
 export * from './lib/auth-guard-service';
