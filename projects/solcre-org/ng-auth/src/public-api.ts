4a15167abe4e6ab45fca2fe7f0f134eca97e34ec
Gustavo Rodriguez <gustavo.rodriguez@solcre.com>
2020-04-27 16:31:21 -0300

    feat(publicApi): export access token model to use it outside

--- projects/solcre-org/ng-auth/src/public-api.ts
+++ projects/solcre-org/ng-auth/src/public-api.ts
@@ -5,3 +5,4 @@ export * from './lib/auth.service';
 export * from './lib/ng-auth.module';
 export * from './lib/auth-guard-service';
 export * from './lib/auth.interceptor'
+export * from './lib/access-token.model'
