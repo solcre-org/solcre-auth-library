f91e806881ef56f87ae023a477e448dfc84e37b6
Gustavo Rodriguez <gustavo.rodriguez@solcre.com>
2021-09-29 16:39:09 -0300

    feat(Angular): update to 12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'solcre-auth-library';
-}
+export class AppComponent {}
